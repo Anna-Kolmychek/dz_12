05e89e04afbd26ee576b2f7fccc0aabdba1dab8a
AnnaKolmychek <a.kolmychek@gmail.com>
2022-12-17 18:58:22 +0300

    refactoring after mentor check

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, render_template, send_from_directory
-from main.views import main_blueprint, search_blueprint
-from loader.views import post_form_blueprint, post_uploaded_blueprint
+from main.views import main_blueprint
+from loader.views import loader_blueprint
 
 
 app = Flask(__name__)
 
 app.register_blueprint(main_blueprint)
-app.register_blueprint(search_blueprint)
-app.register_blueprint(post_form_blueprint, url_prefix='/post')
-app.register_blueprint(post_uploaded_blueprint, url_prefix='/post')
+app.register_blueprint(loader_blueprint, url_prefix='/post')
 
 
 @app.route("/uploads/<path:path>")
